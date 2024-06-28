a555396d6196941e489afed17e7a3863d81c7e79
Christopher Kodama <ckhordiasma@gmail.com>
2024-06-28 13:18:06 +0000

    throwing in tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './**/*.html'
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      typography: {},
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+
+}
